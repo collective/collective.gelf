94b49aea59c0498faa565a61abc9fd482d33c3e9
Asko Soukka <asko.soukka@iki.fi>
2016-04-05 13:57:15 +0300

    Back to development: 0.9.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.gelf',
-    version='0.9.2',
+    version='0.9.3.dev0',
     description='Graylog logging handler for Plone',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.rst').read()),
